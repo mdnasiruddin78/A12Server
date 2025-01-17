8ac3b49d3100a59f2510e68fda76dbef733a7d52
MD.NASIR UDDIN ANIK <nasiruddinanik54@gmail.com>
2025-01-17 17:01:45 +0600

    create admin route jwt

--- index.js
+++ index.js
@@ -27,35 +27,86 @@ async function run() {
 
     const userCollection = client.db("blogsOnlineDb").collection("users");
 
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+      res.send({ token });
+    })
+
+    // middlewares verifyToken
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    // verifyAdmin 
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
     // users related apis
-    app.post('/users',async(req,res) => {
+    app.post('/users', async (req, res) => {
       const user = req.body;
       // insert email if user dosent exist
-      const query = {email: user.email}
+      const query = { email: user.email }
       const existingUser = await userCollection.findOne(query)
-      if(existingUser){
-        return res.send({message: 'user alrady exists',insertedId: null})
+      if (existingUser) {
+        return res.send({ message: 'user alrady exists', insertedId: null })
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
 
     // get all users
-    app.get('/users',async(req,res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      console.log(req.headers)
       const result = await userCollection.find().toArray();
       res.send(result)
     })
 
+    // isAdmin route
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'unauthorized access' })
+      }
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      let admin = false
+      if (user) {
+        admin = user.role === 'admin';
+      }
+      res.send({ admin })
+    })
+
     // make a admin
-    app.patch('/users/admin/:id',async(req,res) => {
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
       const updatedDoc = {
         $set: {
           role: 'admin',
         }
       }
-      const result = await userCollection.updateOne(filter,updatedDoc)
+      const result = await userCollection.updateOne(filter, updatedDoc)
       res.send(result)
     })
 
@@ -71,7 +122,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello from Blog Online Server....')
+  res.send('Hello from Blog Online Server....')
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
