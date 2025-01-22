05879678ea2e1240d84937bb306d4356aeb301e2
MD.NASIR UDDIN ANIK <nasiruddinanik54@gmail.com>
2025-01-22 17:19:49 +0600

    comment function add

--- index.js
+++ index.js
@@ -206,6 +206,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/allComment/:postId',async(req,res) => {
+        const postId = req.params.postId;
+        const query = {postId: postId}
+        const result = await commentCollection.find(query).toArray()
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
