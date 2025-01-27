59ede71cd7a34c692d8bb76e4ba6dd9d0b4d80c1
MD.NASIR UDDIN ANIK <nasiruddinanik54@gmail.com>
2025-01-28 00:21:22 +0600

    report operation done

--- index.js
+++ index.js
@@ -334,6 +334,14 @@ async function run() {
       res.send(result)
     })
 
+    // restriction get by email
+    app.get('/message/:email',verifyToken,async(req,res) => {
+      const email = req.params.email;
+      const query = {email: email}
+      const result = await restrictionCollection.find(query).toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
